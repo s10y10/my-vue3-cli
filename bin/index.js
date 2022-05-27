398dee1c8ed0cb3a201246ba9a71f7dc56ac79aa
s10y10 <248496385@qq.com>
2022-05-27 15:54:01 +0800

    feat: 基本完工,等template完成之后改配置

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,24 @@
 #!/usr/bin/env node
 
-// 请求commander库
 const program = require("commander");
 // 请求 lib/update.js
 const updateChk = require("../lib/update");
+// 请求 lib/mirror.js
+const setMirror = require("../lib/mirror");
+// 请求 lib/download.js
+const dlTemplete = require("../lib/download");
+// 请求 lib/init.js
+const initProject = require("../lib/init");
+
+//init初始化项目
+program
+  .name("my-cli")
+  .usage("<commands [options]>")
+  .command("init <project_name>")
+  .description("Create a javascript plugin project.")
+  .action((project) => {
+    initProject(project);
+  });
 
 // upgrade 检测更新
 // 声明命令
@@ -14,6 +29,22 @@ program
   .description("Check the my-cli version.")
   .action(() => {
     updateChk();
+  });
+
+// mirror 切换镜像链接
+program
+  .command("mirror <template_mirror>")
+  .description("Set the template mirror.")
+  .action((tplMirror) => {
+    setMirror(tplMirror);
+  });
+
+//template 下载/更新模版
+program
+  .command("template")
+  .description("Download template from mirror")
+  .action(() => {
+    dlTemplete();
   });
 
 // 从package.json文件中获取version字段的值，-v和--version是参数
